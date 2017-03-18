8a23d6837be75e2bb83377d58797798cb682e0ab
telematica <telematica>
2017-03-18 11:04:32 -0500

    make it better

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 <head><title>Prueba de App Empresari$</title>
 </head>
 <body>
-  <?php echo '<p> Hola App Empresariales</p> $' ?>
+  <?php echo '<p> Hola App Empresariales</p> ' ?>
+  <p> Lo que quiero colocar </p>
+  
 </body>
 
 </html>
